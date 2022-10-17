90245019d5468d2b58fa146ea813383866e65a1a
OscarPlanas <casplanas7@gmail.com>
2022-10-17 14:48:29 +0200

    SemAngular

---
+++ src/app/models/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    _id?: number;
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+
+
+    constructor(id: number, name: string, email: string, username: string, password: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }   
+}
